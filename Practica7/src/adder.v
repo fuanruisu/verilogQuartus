89f8d3bf6034670c42f1094a5faff3344b10ca9c
Juan Luis Magaña Paz <opomola@gmail.com>
2021-10-20 16:40:56 -0500

    Implementacion full adder en fpga DE10Lite, con 3 swithches y 2 leds

--- Practica7/src/adder.v
+++ Practica7/src/adder.v
@@ -10,17 +10,18 @@ module adder(SW,LEDR);
 //	assign LEDR[1]=in1&in2;
 
 
-input [1:0] SW;
+input [2:0] SW;
 output [1:0] LEDR;
 wire cout, sum;
-wire in1, in2;
+wire in1, in2, cin;
 assign in1=SW[0];
 assign in2=SW[1];
+assign cin=SW[2];
 assign LEDR[0]=cout;
 assign LEDR[1]=sum;
 
-halfAdder(in1, in2, sum, cout);
-
+//halfAdder(in1, in2, sum, cout);
+FullAdder(in1,in2,cin,sum,cout);
 endmodule 
 
 
@@ -30,5 +31,17 @@ input in1,in2;
 output sum, cout;
 assign sum=in1^in2;
 assign cout=in1&in2;
+
+endmodule 
+
+module FullAdder(in1, in2, cin, sum, cout);
+input in1, in2, cin;
+output sum, cout;
+wire w1,w2,w3;
+//                     sum,cout
+//                       ↓  ↓
+halfAdder   HA1(in1,in2, w1,w2);
+halfAdder 	HA2(w1,cin,sum,w3);
+assign cout = w3|w2;
 
 endmodule 
